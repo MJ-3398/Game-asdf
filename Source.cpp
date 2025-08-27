e623acd0f43ab4e9c96d507cb6991882010c89bb
MJ-3398 <g32153851@gmail.com>
2025-08-27 19:54:26 +0900

    update

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
 
@@ -23,23 +25,23 @@ public:
 
 		sf::Vector2f movement(0, 0);
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Left))
-		{
-			movement.x -= speed;
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Right))
-		{
-			movement.x += speed;
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Up))
-		{
-			movement.y -= speed;
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Down))
-		{
-			movement.y += speed;
-		}
-
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Left))
+			{
+				movement.x -= speed;
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Right))
+			{
+				movement.x += speed;
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Up))
+			{
+				movement.y -= speed;
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Down))
+			{
+				movement.y += speed;
+			}
+	
 		shape.move(movement);
 	};
 
@@ -50,16 +52,61 @@ class bullet
 {
 public:
 	sf::RectangleShape shape;
-	float speed = 0.1f;
+	float speeds = 0.9f;
+	bool active = true;
 
-	bullet()
+
+	bullet(float x, float y)
 	{
 		shape.setSize({ 2,2 });
 		shape.setFillColor(sf::Color::Yellow);
-		shape.setPosition({ 500,1200 });
+		shape.setPosition({ x,y });
 	};
 
+	void moving()
+	{
+		shape.move({ 0,-speeds });
+		if (shape.getPosition().y < 0)
+		{
+			active = false;
+		}
+	}
+	bool active1() const
+	{
+		return active;
+	}
+
+	void draw(sf::RenderWindow& window)
+	{
+		if (active)
+		{
+			window.draw(shape);
+		}
+	}
+
 };
+
+class backscreen
+{
+public:
+	sf::RectangleShape star;
+
+	backscreen(int st = 400)
+	{
+		for (int i = 0; i < st; i++)
+		{
+			star.setSize({ 0.2,0.2 });
+			star.setFillColor(sf::Color::Yellow);
+
+			float x = rand() % 1000;
+			float y = rand() % 1400;
+
+			star.setPosition({ x,y });
+		}
+	}
+
+};
+
 
 #pragma region 라이브러리
 // 정적 라이브러리(.lib)
@@ -79,6 +126,7 @@ int main()
 	sf::RenderWindow window(sf::VideoMode({1000,1400}), "SFML Window");
 		
 		ship space;
+		vector<bullet> bullets;
 
 		while (window.isOpen())
 		{
@@ -108,10 +156,25 @@ int main()
 
 			space.Move();
 
-			window.clear(sf::Color::Black);
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Space))
+			{
+				sf::Vector2f pos = space.shape.getPosition();
+				bullets.emplace_back(pos.x + 9, pos.y);
+			}
 
+			for (auto& b : bullets) b.moving();
+
+			bullets.erase(
+				std::remove_if(bullets.begin(), bullets.end(),
+					[](const bullet& b) { return !b.active1(); }),
+				bullets.end());
+
+			window.clear(sf::Color::Black);
+			for (auto& b : bullets) b.draw(window);
 			window.draw(space.shape);
 			window.display();
 		}
+
+
 	return 0;
 }
